7a780db243e4121837460488d850cabf7ce169d3
Jules Lestienne <jules.lestienne@epita.fr>
2024-08-21 19:22:06 +0200

    feat: added hero to render

--- README.md
+++ README.md
@@ -7,16 +7,22 @@ Set. Des questions restent en suspens comme les restrictions autour des Héros,
 si il faut en lister 12 ou 18 et la présence d'unique. Le but sous jacent est
 de mettre en lumière des interactions rares.
 
-## Test render
+## The List
 
+![screenshot](images/hero_axiom.jpg)
 ![screenshot](images/cube_axiom.jpg)
+![screenshot](images/hero_bravos.jpg)
 ![screenshot](images/cube_bravos.jpg)
+![screenshot](images/hero_lyra.jpg)
 ![screenshot](images/cube_lyra.jpg)
+![screenshot](images/hero_muna.jpg)
 ![screenshot](images/cube_muna.jpg)
+![screenshot](images/hero_ordis.jpg)
 ![screenshot](images/cube_ordis.jpg)
+![screenshot](images/hero_yzmir.jpg)
 ![screenshot](images/cube_yzmir.jpg)
 
 ## Ownership of /images content
 
 I do not own any of the arts in the images directory, they belong to Equinox
-Studio creator of Altered TCG.
+Studio, owner of Altered TCG.
--- generate_cube.py
+++ generate_cube.py
@@ -4,6 +4,30 @@ import csv
 from PIL import Image
 
 factions = ["axiom", "bravos", "lyra", "muna", "ordis", "yzmir"]
+hero = {
+    "axiom": ["sierra", "subash", "treyst"],
+    "bravos": ["basira", "kojo"],
+    "lyra": ["fen", "auraq"],
+    "muna": ["teija", "rin", "arjun"],
+    "ordis": ["waru", "sigismar", "gulrang"],
+    "yzmir": ["akesha", "afanas", "lindiwe"]
+    }
+
+def fill_hero_card_list(faction):
+    card_list = []
+    with open(faction + '/list.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=':')
+        line_count = 0
+        for row in csv_reader:
+            card_name = f'{row[0]}_{row[1]}'
+            card_name = card_name.lower()
+            card_name = card_name.replace(" ", "_")
+            card_name = card_name.replace(",", "")
+            card_name = card_name.replace(".", "")
+            for i in range(int(row[2])):
+                card_list.append("images/" + faction + "/" + card_name + ".jpg")
+            line_count += 1
+    return card_list
 
 def fill_car_list_from_faction(faction):
     card_list = []
@@ -21,6 +45,23 @@ def fill_car_list_from_faction(faction):
             line_count += 1
     return card_list
 
+def generate_hero(faction, cards):
+    images = [Image.open("images/" + faction + "/" + x + ".jpg") for x in cards]
+    width = images[0].size[0] * 6
+    height = images[0].size[1] * ((0 if len(cards) % 6 == 0 else 1) + len(cards) // 6)
+    new_im = Image.new('RGB', (width, height))
+    x_offset = 0
+    y_offset = 0
+
+    for i in range(len(cards)):
+        if i % 6 == 0 and i != 0:
+            x_offset = 0
+            y_offset += images[i].size[1]
+        new_im.paste(images[i], (x_offset, y_offset))
+        x_offset += images[i].size[0]
+
+    new_im.save("images/hero_" + faction + ".jpg")
+
 def generate_from_card_list(faction, cards):
     images = [Image.open(x) for x in cards]
     width = images[0].size[0] * 6
@@ -37,7 +78,6 @@ def generate_from_card_list(faction, cards):
         x_offset += images[i].size[0]
 
     new_im.save("images/cube_" + faction + ".jpg")
-
 
 def generate_pdf_from_faction_jpg():
     image_list = []
@@ -59,6 +99,7 @@ def main():
         for faction in factions:
             card_list = fill_car_list_from_faction(faction)
             generate_from_card_list(faction, card_list)
+            generate_hero(faction, hero[faction])
 
     if args.pdf:
         generate_pdf_from_faction_jpg()
