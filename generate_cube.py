5cd747642c645c0bde63ca8d7e26c2ac694c16a0
Jules Lestienne <jules.lestienne@epita.fr>
2024-08-24 11:08:46 +0200

    feat: updated generate_cube

--- generate_cube.py
+++ generate_cube.py
@@ -4,106 +4,77 @@ import csv
 from PIL import Image
 
 factions = ["axiom", "bravos", "lyra", "muna", "ordis", "yzmir"]
-hero = {
-    "axiom": ["sierra", "subash", "treyst"],
-    "bravos": ["basira", "kojo"],
-    "lyra": ["fen", "auraq"],
-    "muna": ["teija", "rin", "arjun"],
-    "ordis": ["waru", "sigismar", "gulrang"],
-    "yzmir": ["akesha", "afanas", "lindiwe"]
-    }
 
-def fill_hero_card_list(faction):
+def get_dimention_from_images(images, col):
+    width = images[0].size[0] * col
+    overflow = (0 if len(images) % col == 0 else 1)
+    height = images[0].size[1] * (overflow + len(images) // col)
+    return (width, height)
+
+
+def card_list_from_file_path(path):
     card_list = []
-    with open(faction + '/list.txt') as csv_file:
+    with open(path) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=':')
-        line_count = 0
         for row in csv_reader:
             card_name = f'{row[0]}_{row[1]}'
-            card_name = card_name.lower()
-            card_name = card_name.replace(" ", "_")
-            card_name = card_name.replace(",", "")
-            card_name = card_name.replace(".", "")
+            card_name = card_name\
+                    .lower()\
+                    .replace(" ", "_")\
+                    .replace(",", "")\
+                    .replace(".", "")
             for i in range(int(row[2])):
-                card_list.append("images/" + faction + "/" + card_name + ".jpg")
-            line_count += 1
+                card_list.append("images/database/" + card_name + ".jpg")
     return card_list
 
-def fill_car_list_from_faction(faction):
-    card_list = []
-    with open(faction + '/list.txt') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=':')
-        line_count = 0
-        for row in csv_reader:
-            card_name = f'{row[0]}_{row[1]}'
-            card_name = card_name.lower()
-            card_name = card_name.replace(" ", "_")
-            card_name = card_name.replace(",", "")
-            card_name = card_name.replace(".", "")
-            for i in range(int(row[2])):
-                card_list.append("images/" + faction + "/" + card_name + ".jpg")
-            line_count += 1
-    return card_list
 
-def generate_hero(faction, cards):
-    images = [Image.open("images/" + faction + "/" + x + ".jpg") for x in cards]
-    width = images[0].size[0] * 6
-    height = images[0].size[1] * ((0 if len(cards) % 6 == 0 else 1) + len(cards) // 6)
-    new_im = Image.new('RGB', (width, height))
+def jpg_from_card_list(name='custom/export.jpg', card_list=[], col=5):
+    images = [Image.open(x) for x in card_list]
+    new_im = Image.new('RGB', get_dimention_from_images(images, col))
     x_offset = 0
     y_offset = 0
 
-    for i in range(len(cards)):
-        if i % 6 == 0 and i != 0:
+    for i in range(len(card_list)):
+        if (i % col == 0) and (i != 0):
             x_offset = 0
             y_offset += images[i].size[1]
         new_im.paste(images[i], (x_offset, y_offset))
         x_offset += images[i].size[0]
 
-    new_im.save("images/hero_" + faction + ".jpg")
+    new_im.save("images/" + name + ".jpg")
 
-def generate_from_card_list(faction, cards):
-    images = [Image.open(x) for x in cards]
-    width = images[0].size[0] * 6
-    height = images[0].size[1] * ((0 if len(cards) % 6 == 0 else 1) + len(cards) // 6)
-    new_im = Image.new('RGB', (width, height))
-    x_offset = 0
-    y_offset = 0
 
-    for i in range(len(cards)):
-        if i % 6 == 0 and i != 0:
-            x_offset = 0
-            y_offset += images[i].size[1]
-        new_im.paste(images[i], (x_offset, y_offset))
-        x_offset += images[i].size[0]
-
-    new_im.save("images/cube_" + faction + ".jpg")
-
-def generate_pdf_from_faction_jpg():
-    image_list = []
-    for faction in factions:
-        image = Image.open(f'images/cube_{faction}.jpg')
+def pdf_from_jpg_path_list(path_list):
+    for path in path_list:
+        image = Image.open(f'{path}')
         image_list.append(image)
+    image_list[0].save('Core_Set.pdf', 'PDF', resolution=100.0, save_all=True, append_images=image_list[1:])
 
-    image_list[0].save('cube.pdf', 'PDF', resolution=100.0, save_all=True, append_images=image_list[1:])
+
+def generate_cube():
+    for faction in factions:
+        card_list = card_list_from_file_path(faction + '/list.txt')
+        jpg_from_card_list('cube_display/' + faction, card_list)
+        card_list = card_list_from_file_path(faction + '/heros.txt')
+        jpg_from_card_list('cube_display/heros_' + faction, card_list)
 
 def main():
     parser = argparse.ArgumentParser(description='jpg and pdf cube generator')
-    parser.add_argument('--pdf', action='store_true', help='generate pdf from\
-                        existing jpg')
-    parser.add_argument('--jpg', action='store_true', help='generate all\
-    faction jpg')
+    parser.add_argument('--full', '-f', dest='full', action='store_true',
+                    help='generate full cube cube')
+    parser.add_argument('--custom', '-c', dest='custom', nargs=3,
+                    metavar=('path_list', 'export_name', 'card_per_line'),
+                    help='generate from card list')
     args = parser.parse_args()
 
-    if args.jpg:
-        for faction in factions:
-            card_list = fill_car_list_from_faction(faction)
-            generate_from_card_list(faction, card_list)
-            generate_hero(faction, hero[faction])
+    if args.custom:
+        card_list = card_list_from_file_path(args.custom[0])
+        jpg_from_card_list(args.custom[1], card_list, int(args.custom[2]))
 
-    if args.pdf:
-        generate_pdf_from_faction_jpg()
+    if args.full:
+        generate_cube()
     return 0
+
 
 if __name__ == "__main__":
     main()
